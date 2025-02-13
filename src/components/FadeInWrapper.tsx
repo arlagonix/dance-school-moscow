7be7a0ad5bb9ebf1f2bd443c99cbbe2feee93c21
Alexander Gorbunov <gorbunov-sasha@yandex.ru>
2025-02-13 18:54:39 +0300

    Minor fixes

--- src/components/FadeInWrapper.tsx
+++ src/components/FadeInWrapper.tsx
@@ -10,10 +10,10 @@ interface ComponentProps {
 const FadeInWrapper = ({ children, delay = 0 }: ComponentProps) => {
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20, scale: 0 }} // start with opacity 0 and slightly shifted down
+      initial={{ opacity: 0, y: -50, scale: 0 }} // start with opacity 0 and slightly shifted down
       whileInView={{ opacity: 1, y: 0, scale: 1 }} // animate to full opacity and original position
       transition={{ delay, duration: 0.5, ease: 'easeOut' }} // customize the transition
-      viewport={{ once: true, amount: 0.5 }} // trigger animation once when 50% of the component is visible
+      viewport={{ once: true, amount: 0.1 }} // trigger animation once when 50% of the component is visible
     >
       {children}
     </motion.div>
