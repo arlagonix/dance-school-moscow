b117f65caa8dd19d092c6f3deadf01ac18b58ba6
Alexander Gorbunov <gorbunov-sasha@yandex.ru>
2025-02-20 10:11:55 +0300

    Updated animations

--- src/components/FadeInWrapper.tsx
+++ src/components/FadeInWrapper.tsx
@@ -16,7 +16,7 @@ const FadeInWrapper = ({ children, delay = 0, className }: ComponentProps) => {
       className={className}
       initial={{ opacity: 0, y: 50, scale: 0.85 }} // start with opacity 0 and slightly shifted down
       whileInView={{ opacity: 1, y: 0, scale: 1 }} // animate to full opacity and original position
-      transition={{ delay, duration: 0.5, ease: 'easeOut' }} // customize the transition
+      transition={{ delay, duration: 0.7, ease: 'easeOut' }} // customize the transition
       viewport={{ once: true, amount: 0.1 }} // trigger animation once when 50% of the component is visible
     >
       {children}
